c63028d0911dcabf54b74cb46fe3a88ef2efb5b1
SpiffyB <50279056+SpiffyB@users.noreply.github.com>
2019-09-06 21:29:35 +0100

    Structure 2

---
+++ chopsticks/__main__.py
@@ -0,0 +1,13 @@
+'''
+Created on 6 Sep 2019
+
+@author: Tom
+'''
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ chopsticks/core.py
@@ -0,0 +1,6 @@
+'''
+Created on 6 Sep 2019
+
+@author: Tom
+'''
+
