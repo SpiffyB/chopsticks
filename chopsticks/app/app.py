85e894b656ecb44e1c9c3ae8e39ceaea6ca6ee31
SpiffyB <50279056+SpiffyB@users.noreply.github.com>
2020-01-08 11:01:10 +0000

    Created basic css file

--- chopsticks/app/app.py
+++ chopsticks/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, flash
 from threading import Thread
 from . import config, forms
 
@@ -11,9 +11,11 @@ def hello_world_route():
     """Print 'Hello, world!' as the response body."""
     return "Hello World"
 
-@app.route('/index')
+@app.route('/index', methods=['GET','POST'])
 def index():
     form = forms.GameForm()
+    if form.validate_on_submit():
+        flash('button pressed {}'.format(form.btn_player1_hand1.data))
     return render_template('index.html', title='Home', user="test_user", form=form)
 
 
